b4936b56e7c34331b90110cf3ea5f6fb498eb4de
Gabryel Venancio <88159869+GabryelVenancio@users.noreply.github.com>
2024-07-01 16:23:29 -0300

    Add files via upload

---
+++ cadastro-cleffs/cadastro (2)/cadastro/cadastro/src/main/java/com/cadastro/cadastro/model/User.java
@@ -0,0 +1,55 @@
+package com.cadastro.cadastro.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id;
+
+    @Column(name = "nome_usuario")
+    private String nome;
+
+    @Column(name = "email_usuario")
+    private String email;
+
+    @Column(name = "senha_usuario")
+    private String senha;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return nome;
+    }
+
+    public void setName(String name) {
+        this.nome = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
